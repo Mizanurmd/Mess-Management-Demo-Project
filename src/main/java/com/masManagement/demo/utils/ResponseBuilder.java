3ba67e19e6cebf7602c1091aadd7cf9bb67caac5
Mizanurmd <mizanur021991@gmail.com>
2023-03-28 23:38:10 +0600

    success response

--- src/main/java/com/masManagement/demo/utils/ResponseBuilder.java
+++ src/main/java/com/masManagement/demo/utils/ResponseBuilder.java
@@ -7,36 +7,54 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 
-
 import com.masManagement.demo.dto.ErrorResponseDto;
 import com.masManagement.demo.dto.Response;
 
 public final class ResponseBuilder {
-	public ResponseBuilder() {}
-	
+	public ResponseBuilder() {
+	}
+
 	//////////// Create a response Builder here ///////////
-	
-	public static List<ErrorResponseDto> getCustomError(BindingResult result){
-		List<ErrorResponseDto>dtoList = new ArrayList<>();
-		result.getFieldErrors().forEach(fieldError ->{
-			ErrorResponseDto dto = ErrorResponseDto.builder()
-					.field(fieldError.getField())
+
+	public static List<ErrorResponseDto> getCustomError(BindingResult result) {
+		List<ErrorResponseDto> dtoList = new ArrayList<>();
+		result.getFieldErrors().forEach(fieldError -> {
+			ErrorResponseDto dto = ErrorResponseDto.builder().field(fieldError.getField())
 					.message(fieldError.getDefaultMessage()).build();
-					dtoList.add(dto);
-			
+			dtoList.add(dto);
+
 		});
-	
+
 		return dtoList;
 	}
-	
+
 /////////// Create fail Response here
-public static Response getFailResponse(HttpStatus status, String message) {
-	return Response.builder()
-		.message(message)
-		.status(status.getReasonPhrase())
-		.statusCode(status.value())
-		.timestamp(new Date().getTime()).build();
-	
-}
+	public static Response getFailResponse(HttpStatus status, String message) {
+		return Response.builder().message(message).status(status.getReasonPhrase()).statusCode(status.value())
+				.timestamp(new Date().getTime()).build();
+
+	}
+
+/////////// Create Success Response here
+	public static Response getSuccessResponse(HttpStatus status, String message, Object content) {
+		return Response.builder().message(message).content(content).statusCode(status.value())
+				.timestamp(new Date().getTime()).build();
+
+	}
+
+//////////Create Success Response here
+	public static Response getSuccessResponse(HttpStatus status, String message, Object content, int numberOfElement) {
+		return Response.builder().message(message).numberOfElement(numberOfElement).statusCode(status.value())
+				.timestamp(new Date().getTime()).build();
+
+	}
+
+//////////Create Success Response here
+	public static Response getSuccessResponse(HttpStatus status, String message, Object content, int numberOfElement,
+			Long rowCount) {
+		return Response.builder().message(message).rowCount(rowCount).statusCode(status.value())
+				.timestamp(new Date().getTime()).build();
+
+	}
 
 }
